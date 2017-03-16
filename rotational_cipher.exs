f8f28f9f87db381e71d366abef80c06a9115879c
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2017-03-16 13:03:01 -0400

    Got this working with a couple additional tests

--- rotational_cipher.exs
+++ rotational_cipher.exs
@@ -8,6 +8,19 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    cond do
+      text == "" or shift == 0 -> text
+      true -> _rotate(to_charlist(text), shift, "")
+    end
+  end
+
+  defp _rotate([], _, acc), do: acc
+  defp _rotate([h|t], shift, acc) when h >= ?a and h <= ?z, do: _rotate(t, shift, acc <> _rotate_character(h, shift, ?a))
+  defp _rotate([h|t], shift, acc) when h >= ?A and h <= ?Z, do: _rotate(t, shift, acc <> _rotate_character(h, shift, ?A))
+  defp _rotate([h|t], shift, acc), do: _rotate(t, shift, acc <> <<h>>)
+
+  defp _rotate_character(character, shift, base_character) do
+    <<rem((character - base_character + shift) , 26) + base_character>>
   end
 end
 
